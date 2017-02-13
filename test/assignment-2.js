13c19c83f1368c803b46f0e13b9f767109528b13
emmanuelv02 <emmanuelv02@hotmail.com>
2017-02-13 08:05:21 -0400

    Change uses of 'request' with 'chai-http' (#3)

--- test/assignment-2.js
+++ test/assignment-2.js
@@ -3,13 +3,11 @@ process.env.NODE_ENV = 'test';
 // deps
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let request = require('request');
+chai.use(chaiHttp);
 
 // fwd declarations
 let should = chai.should();
 let expect = chai.expect;
-
-chai.use(chaiHttp);
 
 // locals
 let host = process.env.HOST;
@@ -17,114 +15,96 @@ let port = process.env.PORT;
 let url = 'http://' + host + ':' + port;
 
 describe('Assignment 2', function () {
+
   describe('/GET 404', function () {
     it('status code for 404 should be 404', function (done) {
-      var options = {
-        url: url + '/404'
-      };
-
-      request.get(options, function (err, res, body) {
-        expect(res.statusCode).to.equal(404);
-        done();
-      });
+        chai.request(url)
+            .get('/404')
+            .end(function (err, res) {
+                expect(res.statusCode).to.equal(404);
+                done();
+            });
     });
   });
 
   describe('/GET protected', function () {
     it('status code for /protected should be 401', function (done) {
-      var options = {
-        url: url + '/protected'
-      };
-
-      request.get(options, function (err, res, body) {
-        expect(res.statusCode).to.equal(401);
-        done();
-      });
+        chai.request(url)
+            .get('/protected')
+            .end(function (err, res) {
+                expect(res.statusCode).to.equal(401);
+                done();
+            });
     });
   });
 
   describe('/GET 500', function () {
     it('status code for /error should be 500', function (done) {
-      var options = {
-        url: url + '/error'
-      };
-
-      request.get(options, function (err, res, body) {
-        expect(res.statusCode).to.equal(500);
-        done();
-      });
+        chai.request(url)
+            .get('/error')
+            .end(function (err, res) {
+                expect(res.statusCode).to.equal(500);
+                done();
+            });
     });
   });
 
   describe('/PATCH GET POST PUT notimplemented', function () {
     it('status code for PATCH should be 501', function (done) {
-      var options = {
-        url: url + '/notimplemented'
-      };
-
-      request.patch(options, function (err, res, body) {      
-        res.headers.should.have.property('allow');
-        expect(res.statusCode).to.equal(501);
-        done();
-      });
+        chai.request(url)
+            .patch('/notimplemented')
+            .end(function (err, res) {
+                res.headers.should.have.property('allow');
+                expect(res.statusCode).to.equal(501);
+                done();
+            });
     });
 
     it('status code for GET should be 200', function (done) {
-      var options = {
-        url: url + '/notimplemented'
-      };
-
-      request.get(options, function (err, res, body) {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+        chai.request(url)
+            .get('/notimplemented')
+            .end(function (err, res) {
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
     });
 
     it('status code for POST should be 200', function (done) {
-      var options = {
-        url: url + '/notimplemented'
-      };
 
-      request.post(options, function (err, res, body) {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+        chai.request(url)
+            .post('/notimplemented')
+            .end(function (err, res) {
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
     });
 
     it('status code for PUT should be 200', function (done) {
-      var options = {
-        url: url + '/notimplemented'
-      };
-
-      request.put(options, function (err, res, body) {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+        chai.request(url)
+            .put('/notimplemented')
+            .end(function (err, res) {
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
     });
   });
 
-describe('Login', function () {  
+describe('Login', function () {
   describe('/GET login', function () {
     it('content-type should be text/html', function (done) {
-      var options = {
-        url: url + '/login'
-      };
-
-      request.get(options, function (err, res, body) {
-        expect(res.statusCode).to.equal(200);
-        expect(res.headers['content-type']).contains('text/html');
-        done();
-      });
+        chai.request(url)
+            .get('/login')
+            .end(function (err, res) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.headers['content-type']).contains('text/html');
+                done();
+            });
     });
   });
 
   describe('/POST login', function () {
       it('login should return valid JSON object', function (done) {
-        var options = {
-          url: url + '/login'
-        };
-
-        let login = { 
+        let login = {
             username: 'bartolo',
             password: 'colon'
         };
